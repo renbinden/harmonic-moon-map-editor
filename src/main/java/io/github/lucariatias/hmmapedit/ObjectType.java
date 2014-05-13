8ca361ba2d98a016ebb54a4cafa71b210cfe104a
Lucariatias <lucariatias@gmail.com>
2014-05-13 19:11:46 +0100

    Object enum updates

--- src/main/java/io/github/lucariatias/hmmapedit/ObjectType.java
+++ src/main/java/io/github/lucariatias/hmmapedit/ObjectType.java
@@ -6,7 +6,16 @@ public enum ObjectType {
 
     NOTHING(0, 0, 0),
     BLOCK(0, 0, 1),
-    LONYRE(0, 0, 2);
+    DOOR(0, 0, 2),
+    LONYRE(0, 1, 0),
+    TIVOR(0, 1, 1),
+    KESOWA(0, 1, 2),
+    NAMAPO(0, 1, 3),
+    SYALAE(0, 1, 4),
+    ANARIA(0, 1, 5),
+    IDAIN(0, 1, 6),
+    SEURI(0, 1, 7);
+
 
     private final int r;
     private final int g;
